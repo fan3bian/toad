ad20116537152ac856d30eeca721cb1f3d967cbb
zhangsy <908799521@qq.com>
2017-10-20 23:36:26 +0800

    接口以及pom文件

--- toad/src/main/java/io/bio/TestIO.java
+++ toad/src/main/java/io/bio/TestIO.java
@@ -1,8 +1,14 @@
 package io.bio;
 
+import java.io.InputStream;
+import java.io.OutputStream;
+
+import org.junit.Test;
+
 public class TestIO {
-	
-	public void testStream(){
-		
+	@Test
+	public void testStream() {
+		InputStream inStream = null;
+		OutputStream outputStream = null;
 	}
 }
