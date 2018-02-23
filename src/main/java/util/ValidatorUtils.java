e0e9f0e36f962833c05be506eab85ee563f58cb7
zhangshuyi1 <zhangshuyi1@jd.com>
2018-02-23 21:56:43 +0800

    some thing

--- src/main/java/util/ValidatorUtils.java
+++ src/main/java/util/ValidatorUtils.java
@@ -10,7 +10,13 @@ import java.util.regex.Pattern;
  * Created by zhangshuyi1 on 2018/1/18.
  */
 public class ValidatorUtils {
-
+    /**
+     * null ' '
+     * 字符串格式校验
+     * @param regex
+     * @param target
+     * @return
+     */
     public static boolean validateCharSequence(String regex, CharSequence target) {
         return StringUtils.isNotBlank(target) && Pattern.compile(regex).matcher(target).matches();
     }
