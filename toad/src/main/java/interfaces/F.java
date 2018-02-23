0c432792684c1261ff412d5532fba4e27bb724b8
zhangshuyi1 <zhangshuyi1@jd.com>
2018-02-23 09:51:37 +0800

    rm toad

--- toad/src/main/java/interfaces/F.java
+++
@@ -1,5 +0,0 @@
-package interfaces;
-
-public interface F {
-	void close() throws Exception;
-}
--- toad/src/main/java/interfaces/S.java
+++
@@ -1,5 +0,0 @@
-package interfaces;
-
-public interface S extends F{
-	public void close();//覆盖了父接口中的close方法
-}
