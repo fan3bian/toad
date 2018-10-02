2f527c078d5b64dd03c038deface9d4386fe0cca
zhangsy <908799521@qq.com>
2018-10-02 16:17:29 +0800

    regular commmit

---
+++ src/main/java/concurrency/thread/Conuting.java
@@ -0,0 +1,14 @@
+package concurrency.thread;
+
+public class Conuting {
+    class Counter{
+        int count = 0;
+        void increment(){
+            count++;
+        }
+        int getCount(){
+            return  count;
+        }
+    }
+
+}
---
+++ src/main/java/concurrency/thread/TestThread.java
@@ -0,0 +1,19 @@
+package concurrency.thread;
+
+public class TestThread {
+    public static void main(String[] args) throws InterruptedException {
+        Thread thread = new Thread(){
+            @Override
+            public void run() {
+                for(int i =0;i<100;i++)
+                System.out.println("new Thread");
+            }
+        };
+        thread.start();
+        Thread.yield();
+        System.out.println("main Thread");
+        thread.join();
+        System.out.println("ending");
+    }
+
+}
