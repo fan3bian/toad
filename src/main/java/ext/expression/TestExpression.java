2b69ee8e071fbd663438a33f11f02694f8326526
zhangshuyi1 <zhangshuyi1@jd.com>
2018-10-27 10:05:07 +0800

    regex

--- src/main/java/ext/expression/TestExpression.java
+++ src/main/java/ext/expression/TestExpression.java
@@ -1,6 +1,12 @@
 package ext.expression;
 
 import org.apache.commons.jexl3.*;
+import org.junit.Test;
+import util.ValidatorUtils;
+
+import java.math.BigDecimal;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  * Created by zhangshuyi1 on 2018/7/25.
@@ -8,16 +14,38 @@ import org.apache.commons.jexl3.*;
  */
 public class TestExpression {
     private static JexlEngine engine = new JexlBuilder().silent(false).strict(true).create();
-
+    public final static String AMOUNT = "^([0-9]+|[0-9]{1,3}(,[0-9]{3})*)(.[0-9]{1,2})?$";
+    public final static String AMOUNT2 = "^-?\\\\d+(\\\\.\\\\d+)?$";
+    public final String positiveFloat = "^(([0-9]+\\.[0-9]*[1-9][0-9]*)|([0-9]*[1-9][0-9]*\\.[0-9]+)|([0-9]*[1-9][0-9]*))$";
+    public final String non_positiveF="^\\d+(\\.\\d+)?$";
     public static void main(String[] args) {
 
         String exprStr=" val>=1000&&val<=2000 ";
 
         JexlExpression expr = engine.createExpression(exprStr);
         JexlContext ctx = new MapContext();
-        ctx.set("val",1200);
+        ctx.set("val",new BigDecimal("1010.11"));
 
         System.out.println(expr.evaluate(ctx));
 
     }
+    @Test
+    public void test1(){
+        Pattern pattern = Pattern.compile(AMOUNT2);
+        Matcher matcher = pattern.matcher("123122");
+
+        System.out.println(matcher.matches());
+        System.out.println(pattern.matcher("0").matches());
+
+    }
+    @Test
+    public void test2(){
+
+        System.out.println(ValidatorUtils.validateCharSequence(non_positiveF,"-12312.1231"));
+        System.out.println(ValidatorUtils.validateCharSequence(non_positiveF,"0.0"));
+        System.out.println(ValidatorUtils.validateCharSequence(non_positiveF,"0"));
+        System.out.println(ValidatorUtils.validateCharSequence(non_positiveF,"123123"));
+        System.out.println(ValidatorUtils.validateCharSequence(non_positiveF,"1231.1231231231"));
+    }
+
 }
--- src/main/java/ext/json/TestGson.java
+++ src/main/java/ext/json/TestGson.java
@@ -1,14 +1,23 @@
 package ext.json;
 
-import com.google.gson.*;
+import com.google.gson.Gson;
+import com.google.gson.JsonNull;
+import com.google.gson.reflect.TypeToken;
+import org.codehaus.jackson.type.TypeReference;
 import org.junit.Test;
+import util.JsonUtil;
+
+import java.io.IOException;
+import java.util.Map;
 
 public class TestGson {
     @Test
     public void tesSerialize(){
+        Object o = null;
         Gson gson = new Gson();
         System.out.println(gson.toJson(1));
         System.out.println(gson.toJson("hha"));
+        System.out.println(gson.toJson(o));
         System.out.println(gson.toJson(null));
         System.out.println(gson.toJson(new Object()));
         System.out.println(gson.toJson(new String[2]));
@@ -17,6 +26,16 @@ public class TestGson {
 //        e.getAsBoolean();
         JsonNull jn = new JsonNull();
         System.out.println(jn.hashCode());
+
+    }
+
+    public static void main(String... args) throws IOException {
+
+          Map<String, String> remarkMap = JsonUtil.fromJsonByGoogle("", new TypeToken<Map<String, String>>() {
+            });
+          Map<String, String> jacksonMap = JsonUtil.fromJson("", new TypeReference<Map<String, String>>() {
+        });
+
 
     }
 }
