58eb1771986d759dcca3a12a9943055b46fe4842
zhangshuyi1 <zhangshuyi1@jd.com>
2018-11-07 11:34:03 +0800

    reflection

---
+++ src/main/java/advanced/reflect/TestReflect.java
@@ -0,0 +1,13 @@
+package advanced.reflect;
+
+/**
+ * Created by zhangshuyi1 on 2018/10/4.
+ */
+public class TestReflect {
+    public static void main(String[] args) {
+        int a = 0x00002000;
+        int b = 0x00001000;
+        System.out.println(b);//0x表示16进制
+
+    }
+}
