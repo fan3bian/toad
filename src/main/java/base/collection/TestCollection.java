34910f714d2e54f8ad23c276ca712c15528540f9
zhangsy <908799521@qq.com>
2018-11-18 11:51:43 +0800

    regular commmit

---
+++ src/main/java/base/collection/TestCollection.java
@@ -0,0 +1,13 @@
+package base.collection;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class TestCollection {
+    public static void main(String[] args) {
+        List list =new ArrayList<>();
+        list.add(1);
+        list.add(0,0);
+        
+    }
+}
