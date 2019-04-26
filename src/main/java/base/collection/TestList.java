8fb1dc83bb8e79a1c1304bf6bf68340a4a838b92
zhangsy <908799521@qq.com>
2019-04-26 21:51:26 +0800

    regular commmit

---
+++ src/main/java/base/collection/TestList.java
@@ -0,0 +1,12 @@
+package base.collection;
+
+import org.junit.Test;
+
+import java.util.ArrayList;
+
+public class TestList{
+    @Test
+    public void test(){
+        new ArrayList<String>();
+    }
+}
