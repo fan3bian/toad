a1140ef854d90d84bbf7e6e5130c57d9f8d01d8e
zhangsy <908799521@qq.com>
2020-11-01 18:25:33 +0800

    regular

---
+++ src/main/java/algorithm/leetcode/array/medium/Test75.java
@@ -0,0 +1,29 @@
+package algorithm.leetcode.array.medium;
+
+public class Test75 {
+    //变量：初始值 变化规律
+    public void sortColors(int[] nums) {
+        // 对于所有 idx < i : nums[idx < i] = 0
+        // j是当前考虑元素的下标
+        int p0 = 0, curr = 0;
+        // 对于所有 idx > k : nums[idx > k] = 2
+        int p2 = nums.length - 1;
+
+        int tmp;
+        while (curr <= p2) {
+            if (nums[curr] == 0) {
+                // 交换第 p0个和第curr个元素
+                // i++，j++
+                tmp = nums[p0];
+                nums[p0++] = nums[curr];
+                nums[curr++] = tmp;
+            } else if (nums[curr] == 2) {
+                // 交换第k个和第curr个元素
+                // p2--
+                tmp = nums[curr];
+                nums[curr] = nums[p2];
+                nums[p2--] = tmp;
+            } else curr++;
+        }
+    }
+}
