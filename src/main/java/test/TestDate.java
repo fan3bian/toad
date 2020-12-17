0486d615fdddce1f329a6cae5628bcef5cf0b645
zhangshuyi1 <zhangshuyi1@jd.com>
2020-12-17 18:08:56 +0800

    move the package

--- src/main/java/test/TestDate.java
+++ src/main/java/test/TestDate.java
@@ -1,7 +1,11 @@
 package test;
 
+import com.alibaba.fastjson.JSON;
 import org.junit.Test;
 import util.DateUtil;
+import util.JsonUtil;
+
+import java.util.Date;
 
 /**
  * Created by zhangshuyi1 on 2018/8/31.
@@ -15,5 +19,6 @@ public class TestDate {
         System.out.println(after);
         byte a = 3;
         System.out.println(a==3);
+        System.out.println(JSON.toJSONString(new Date()));
     }
 }
