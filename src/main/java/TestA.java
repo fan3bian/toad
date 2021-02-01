5a095179bdac97cb5541797d093572b65b85d416
zhangshuyi1 <zhangshuyi1@jd.com>
2021-02-01 11:23:57 +0800

    add

--- src/main/java/TestA.java
+++ src/main/java/TestA.java
@@ -11,10 +11,9 @@ public class TestA {
 
 
         String[] all = {
-                "KDJJH700002101220014","KDJJH700002101220016","KDJJH700002101220017","KDJJH700002101220010","KDJJH700002101220012","KDJJH700002101220004","KDJJH700002101220005","KDJJH900002101220006","KDJJH700002101220008","KDJJH700002101220013","KDJJH700002101220009","KDJJH700002101220015","KDJJH700002101220007","KDJJH700002101220011","KDJJH700002101220053","LSJH700002101220055","KDJJH700002101220054","PPJH700002101220142","KDJJH700002101230007","SDJJH900002101230012","SDJJH900002101230036","V70000210123030","PPJH700002101230105","XYTJH700002101230059"
+
         };
         String[] split ={
-                "KDJJH700002101220012","KDJJH700002101220013","KDJJH700002101220014","KDJJH700002101220016","KDJJH700002101220008","KDJJH700002101220010","KDJJH700002101220017","KDJJH700002101220054","KDJJH700002101220007","KDJJH700002101230007","KDJJH700002101220009","KDJJH700002101220011","KDJJH700002101220015","KDJJH700002101220053","PPJH700002101220142","LSJH700002101220055","V70000210123030","SDJJH900002101230036","PPJH700002101230105","XYTJH700002101230059"
         };
 
         List<String> dbs = Arrays.asList(split);
@@ -26,15 +25,21 @@ public class TestA {
 
     @Test
     public void test() {
-        String[] a = {};
-        String[] b = {};
+        String[] a = {
+
+        };
+        String[] b = {
+
+        };
+
         List<String> aList = Arrays.asList(a);
         List<String> bList = Arrays.asList(b);
         HashSet<String> strings = new HashSet<>();
-        strings.addAll(aList);
-        strings.addAll(bList);
-        System.out.println(strings);
-
+        List<String> unContains = bList.stream().filter(x -> !aList.contains(x)).collect(Collectors.toList());
+        System.out.println(unContains.size());
+        System.out.println("--\n");
+        List<String> contains = bList.stream().filter(x -> aList.contains(x)).collect(Collectors.toList());
+        System.out.println(contains);
 
     }
 }
